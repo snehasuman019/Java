c883ee6028699cd5480aafbe7e029cd0e4e15f5b
[sneha suman] <snehasuman1119@gmail.com>
2026-01-22 18:24:37 +0530

    Some ques

---
+++ 01.Basics/InventorySystem.java
@@ -0,0 +1,73 @@
+//package 01.Basics;
+
+//you own a inventory system , you have n products 
+// -for each product input current stock and minimum required stock 
+// -if any product does not meets the required stock then print "Restock needed for product x"
+// where x is the product number 
+// print the total number of product that needs to be restocked"
+// 
+/* 
+import java.util.Scanner;
+
+public class InventorySystem{
+    public static void main(String[] args){
+        int n, cur_stock, req_stock, count =0;
+        Scanner sc = new Scanner(System.in);             //
+        System.out.println("Enter number of products: ");
+        int a = sc.nextInt();
+        for(int i =0;i<0;i++){
+            System.out.println("Current stock of product " + (i+1) + ": ");
+            cur_stock = sc.nextInt();
+            System.out.println("Restock needed for product " + (i+1) + ": ");
+            req_stock = sc.nextInt();
+            if(cur_stock < req_stock){
+                System.out.println("Restock needed for product " + (i+1));
+                count++;
+
+        }
+        else{
+            System.out.println("Requirement fullfiled");
+                }
+
+
+    }
+    System.out.println("Total products to be restocked: " + count);
+    sc.close();
+}
+}
+*/
+
+
+//take an input amount from the user , you have to divide this amount into higher denominations as possible of 2000, 500, and 100. 
+// try to take the least number of notes possible
+
+
+import java.util.Scanner;
+
+public class InventorySystem{
+    public static void main(String[] args){
+        int amount;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount: ");
+        amount = sc.nextInt();
+        if (amount % 100 != 0) {
+            System.out.println("Amount cannot be dispensed using 2000, 500 and 100 notes only.");
+            return;
+        }
+        int notes2000 = amount / 2000;
+        amount = amount % 2000;
+        int notes500 = amount /500;
+        amount = amount % 500;
+        int notes100 = amount /100;
+        amount = amount % 100;
+        System.out.println("Number of 2000 notes:"+notes2000);
+        System.out.println("Number of 500 notes:"+notes500);
+        System.out.println("Number of 100 notes:"+notes100);
+        sc.close();
+
+    }
+}
+
+
+// write a progeam to print sum of numbers taken by user . NOTE : you have to take input till user enters 0 and then give the sum of all numbers
+// 
--- 01.Basics/ParkingBill.java
+++ 01.Basics/ParkingBill.java
@@ -6,18 +6,42 @@ public class ParkingBill {
 //first 2 hrs = 50/h
 //next 3 hrs = 25/h
 //..remaining hrs = 10/h
-        float hours = 7;
-        float bill = 0;
-        if(hours <= 2){
-            bill = hours*50;
-        }
-        else if(hours <=5){
-            bill = 2*50 + (hours - 2)*25;
+        // float hours = 7;
+        // float bill = 0;
+        // if(hours <= 2){
+        //     bill = hours*50;
+        // }
+        // else if(hours <=5){
+        //     bill = 2*50 + (hours - 2)*25;
+        // }
+        // else{
+        //     bill = 2*50 + 3*25 + (hours -5)*10;
+        // System.out.println("The parking bill is: " + bill);
+        
+        // }
+
+//electicity bill
+//0-100 unit 2rup/unit
+//101-200 unit 3rup/unit
+//rest 4rup/unit
+//fixed charge = 50rup
+//if bill excedes 2000 pay on extra 10% fine
+        int unit = 105;
+        int bill = 0;
+        if(unit <= 100){
+            bill = unit*2 + 50;
+        }else if(unit <= 200){
+            bill = 100*2 + (unit -100)*3 + 50;
+
         }
         else{
-            bill = 2*50 + 3*25 + (hours -5)*10;
-        System.out.println("The parking bill is: " + bill);
-        
+            bill = 100*2 + 100*3 + (unit -200)*4 + 50;
+            if(bill > 2000){
+                bill += bill/10;
+            }
         }
+        System.out.println("The electricity bill is: " + bill);
+
+
         }
 }
