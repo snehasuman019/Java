ca4ab24da0a4505781577a970ba747e392525c16
[sneha suman] <snehasuman1119@gmail.com>
2026-01-25 21:32:09 +0530

    Polymorphism using int and float

--- Day 03/Polymorphi.java
+++ Day 03/Polymorphi.java
@@ -18,6 +18,8 @@
 // }
 // try considering an int and a float here.
 
+
+/* 
 class A {
     void fun1() {
         System.out.println("Hi (from Class A)");
@@ -39,5 +41,31 @@ public class Polymorphi {
         
         // This will trigger the overridden method in Class B
         b.fun1();
+    }
+}
+*/
+
+
+
+class Calculator {
+    // Method for integer addition
+    void add(int a, int b) {
+        System.out.println("Sum of integers: " + (a + b));
+    }
+
+    // Overloaded method for float addition
+    // The method name is the same, but the parameter types are different
+    void add(float a, float b) {
+        System.out.println("Sum of floats: " + (a + b));
+    }
+}
+
+public class Polymorphi {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        // Java knows which one to call based on the arguments
+        calc.add(10, 20);          // Calls the int version
+        calc.add(10.5f, 20.3f);    // Calls the float version (note the 'f' suffix)
     }
 }
