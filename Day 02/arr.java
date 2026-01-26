4aa61cccfc34c677aef5a7286e75dc4a8653c324
[sneha suman] <snehasuman1119@gmail.com>
2026-01-26 12:31:45 +0530

    Arrays

---
+++ Day 02/arr.java
@@ -0,0 +1,11 @@
+// arrays in java
+public class arr {
+    public static void main(String[] args){
+        //initializiation
+        int[] arr = {40,55,63,17,22};
+        //size
+        int n = arr.length;
+        for (int i=0;i<n;i++)
+            System.out.println(arr[i]+" ");
+    }
+}
