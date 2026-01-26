ef785824143b0115b3172fc002c23cb90a7b9c34
[sneha suman] <snehasuman1119@gmail.com>
2026-01-26 13:13:46 +0530

    Array of object in java

--- Day 02/arr.java
+++ Day 02/arr.java
@@ -15,7 +15,7 @@ public class arr {
     }
 }
 */
-
+/* 
 public class arr{
     public static void main(String[] args){
         int[] arr;
@@ -29,4 +29,29 @@ public class arr{
             System.out.println("Element at index " + i + " : " + arr[i]);
         }
      }
+}
+     */
+//array object 
+class Student{
+    public int roll_no;
+    public String name;
+    Student(int roll_no, String name){
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
+public class arr{
+    public static void main(String[] args){
+        Student[] arr;
+        arr = new Student[5];
+        arr[0]= new Student(101,"Alice");
+        arr[1]= new Student(102,"Bob"); 
+        arr[2]= new Student(103,"Charlie");
+        arr[3]= new Student(104,"David");
+        arr[4]= new Student(105,"Eve");
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Element at index " + i + " : " + arr[i].roll_no + " " + arr[i].name);
+
+        }
+    }
 }
