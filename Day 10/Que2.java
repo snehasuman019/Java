59c1a3caa9c350453e6ed0ec6055bed842b1fc82
[sneha suman] <snehasuman1119@gmail.com>
2026-02-08 23:07:43 +0530

    Questions

---
+++ Day 10/Que2.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+public class Que2 {
+    public static void main(String[] args){
+        
+    }
+
+}
---
+++ Day 10/hollowtri.java
@@ -0,0 +1,13 @@
+
+
+import java.util.*;
+public class hollowtri {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;i<=n;j++){
+                
+            }
+        }
+    }
+}
